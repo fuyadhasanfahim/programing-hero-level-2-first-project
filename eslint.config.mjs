b51d84ea7ff22c529d4b930dea730d76dff75eba
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2024-09-09 19:35:37 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default [
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        ignores: ['node_modules', 'dist', '*.config.js'],
+    },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            'no-unused-vars': 'error',
+            'no-unused-expressions': 'error',
+            'prefer-const': 'error',
+            'no-console': 'warn',
+            'no-undef': 'error',
+        },
+        globals: {
+            process: 'readonly',
+        },
+        extends: [
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'prettier',
+        ],
+    },
+]
