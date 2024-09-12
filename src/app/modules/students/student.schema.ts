86395737a479f910e9df085d84d913212295f80f
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2024-09-12 19:34:15 +0600

    Modified the student shema file.

--- src/app/modules/students/student.schema.ts
+++ src/app/modules/students/student.schema.ts
@@ -9,53 +9,58 @@ const UserNameSchema = new Schema({
 const GuardianSchema = new Schema({
     fathersName: {
         type: String,
-        required: [true, 'Fathers name is required.'],
+        required: [true, "Father's name is required."],
     },
     fathersOccupation: {
         type: String,
-        required: [true, 'Fathers occupation is required.'],
+        required: [true, "Father's occupation is required."],
     },
     fathersContactNo: {
         type: String,
-        required: [true, 'Fathers contact number is required.'],
+        required: [true, "Father's contact number is required."],
     },
     mothersName: {
         type: String,
-        required: [true, 'Mothers name is required.'],
+        required: [true, "Mother's name is required."],
     },
     mothersOccupation: {
         type: String,
-        required: [true, 'Mothers occupation is required.'],
+        required: [true, "Mother's occupation is required."],
     },
     mothersContactNo: {
         type: String,
-        required: [true, 'Mothers contact number is required.'],
+        required: [true, "Mother's contact number is required."],
     },
 })
 
 const LocalGuardianSchema = new Schema({
     name: {
         type: String,
-        required: [true, 'Local guardians name is required.'],
+        required: [true, "Local guardian's name is required."],
     },
     occupation: {
         type: String,
-        required: [true, 'Local guardians occupation is required.'],
+        required: [true, "Local guardian's occupation is required."],
     },
     contactNo: {
         type: String,
-        required: [true, 'Local guardians contact number is required.'],
+        required: [true, "Local guardian's contact number is required."],
     },
 })
 
 const StudentSchema = new Schema({
-    id: { type: String, required: [true, 'ID is required.'], unique: true },
-    name: { type: UserNameSchema, required: [true, 'Name is required.'] },
+    id: {
+        type: String,
+        required: [true, 'Student ID is required.'],
+        unique: true,
+    },
+    name: { type: UserNameSchema, required: [true, 'Full name is required.'] },
     gender: {
         type: String,
         enum: {
             values: ['Male', 'Female', 'Other'],
-            message: `{VALUE} is not a valid gender. Gender must be either Male, Female, or Other.`,
+            message:
+                '{VALUE} is not valid. Gender must be Male, Female, or Other.',
         },
         required: [true, 'Gender is required.'],
     },
@@ -91,17 +96,17 @@ const StudentSchema = new Schema({
     },
     guardian: {
         type: GuardianSchema,
-        required: [true, 'Guardian is required.'],
+        required: [true, 'Guardian information is required.'],
     },
     localGuardian: {
         type: LocalGuardianSchema,
-        required: [true, 'Local guardian is required.'],
+        required: [true, 'Local guardian information is required.'],
     },
     profileImage: { type: String, required: false },
     isActive: {
         type: String,
         enum: ['active', 'inactive'],
-        required: [true, 'isActive field is required.'],
+        required: [true, 'Student status (active/inactive) is required.'],
         default: 'active',
     },
 })
